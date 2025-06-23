5c6f63b820c2d23811907bf3b550c07da3be07e4
hiranaru <n-hirano@sbh-sendai.com>
2025-06-23 14:02:19 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import './index.css'; // ←一番上か下に追加すればOK！
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
