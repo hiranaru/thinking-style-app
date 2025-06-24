178b85539849bf6ea5d3e194866252a87289ecc6
hiranaru <n-hirano@sbh-sendai.com>
2025-06-24 21:27:09 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get, runTransaction } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUrJd6ZXdUZ3x5Q_KrNRAfI10_XM0zE5s",
+  authDomain: "sample-firebase-ai-app-e2b6c.firebaseapp.com",
+  projectId: "sample-firebase-ai-app-e2b6c",
+  storageBucket: "sample-firebase-ai-app-e2b6c.firebasestorage.app",
+  messagingSenderId: "1023831833694",
+  appId: "1:1023831833694:web:a2c86c60dcfc75a8a248cf",
+  databaseURL: "https://sample-firebase-ai-app-e2b6c-default-rtdb.firebaseio.com"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, get, runTransaction };
