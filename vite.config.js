aadf6bf97da2a03bede5993e048512128eeb6e1e
hiranaru <n-hirano@sbh-sendai.com>
2025-06-24 22:04:30 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
