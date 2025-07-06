04201d69885e0895fac4faee721b84db81e67b2c
hiranaru <n-hirano@sbh-sendai.com>
2025-07-06 22:13:37 +0900

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -10,7 +10,7 @@ export default function StartScreen({ onStart }) {
         思考スタイル診断！
       </h1>
             <img
-        src="public/logo.png"
+        src="/logo.png"
         alt="診断タイトルロゴ"
         className="w-[80%] max-w-[320px] mb-6"
       />
